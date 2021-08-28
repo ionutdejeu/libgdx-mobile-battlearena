78589b69ec7c3fcfdc72975693d8007b0c7a3ac9
ionutdejeu <ionut.dejeu@gmail.com>
2021-08-28 13:02:35 +0300

    World

--- core/src/com/libgdx/battlearena/game/BattleArena.java
+++ core/src/com/libgdx/battlearena/game/BattleArena.java
@@ -1,44 +1,115 @@
 package com.libgdx.battlearena.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.libgdx.battlearena.GUI.Controller;
+
+import java.net.URI;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
 
 public class BattleArena extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	private Socket socket;
+	public static final float PPM = 100;
+	public static SpriteBatch batch;
+	World world;
+	float lerp = 0.1f;
+	OrthographicCamera cam;
+	Viewport viewport;
+	Box2DDebugRenderer b2dr;
+	Body player;
+	Controller controller;
+
 	@Override
 	public void create () {
+		cam = new OrthographicCamera();
+
+		viewport = new FitViewport(800 / PPM, 480 / PPM, cam);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		connectSocket();
+		world = new World(new Vector2(0, 0), true);
+		b2dr = new Box2DDebugRenderer();
+		createGround();
+		createPlayer();
+		controller = new Controller(batch);
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+		viewport.update(width, height);
+		controller.resize(width, height);
+	}
+	public void handleInput(){
+		if(controller.touchStarted()){
+			Vector2 s = controller.speed().nor();
+			System.out.println(s);
+			player.setLinearVelocity(s);
+		}
+
+	}
+
+	public void update(float dt){
+		handleInput();
+		world.step(dt, 6, 2);
+		Vector3 position = cam.position;
+		position.x += (player.getPosition().x - position.x) * lerp * dt;
+		position.y += (player.getPosition().y - position.y) * lerp * dt;
+		//cam.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
+
+		cam.update();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		update(Gdx.graphics.getDeltaTime());
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		b2dr.render(world, cam.combined);
+		if(Gdx.app.getType() == Application.ApplicationType.Android)
+			controller.draw();
 	}
 
-	private void connectSocket(){
-		try{
-			socket = IO.socket("ws://localhost:3000");
-			socket.connect();
-		}catch (Exception e){
-			System.out.println(e.getMessage());
-		}
+	public void createGround(){
+		BodyDef bdef = new BodyDef();
+		bdef.position.set(viewport.getWorldWidth() / 2, 0);
+		bdef.type = BodyDef.BodyType.StaticBody;
+		Body b2body = world.createBody(bdef);
+
+		FixtureDef fdef = new FixtureDef();
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(viewport.getWorldWidth() / 2, 20 / PPM);
+
+		fdef.shape = shape;
+		b2body.createFixture(fdef);
+	}
+	public void createPlayer(){
+		BodyDef bdef = new BodyDef();
+		bdef.position.set(viewport.getWorldWidth() / 2, 80 / PPM);
+		bdef.type = BodyDef.BodyType.DynamicBody;
+		player = world.createBody(bdef);
+
+		FixtureDef fdef = new FixtureDef();
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(40 / PPM, 40 / PPM);
+
+		fdef.shape = shape;
+		player.createFixture(fdef);
 	}
 }
